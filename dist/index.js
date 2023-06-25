e4a047e4b582f1980459c1c26087847994b74c71
Julien Lengrand-Lambert <julien@lengrand.fr>
2023-06-25 15:05:32 +0200

    don't forget to build

--- dist/index.js
+++ dist/index.js
@@ -9793,9 +9793,14 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 try {
+
+  const token = core.getInput('access-token');
+  console.log(`The token is ${token}!`);
+
+
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
