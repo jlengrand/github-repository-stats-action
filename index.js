17eecf978e9f48514bf7d12e23d7dc123973c6d0
Julien Lengrand-Lambert <julien@lengrand.fr>
2023-06-25 15:01:13 +0200

    Testing access token

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+
+  const token = core.getInput('access-token');
+  console.log(`The token is ${token}!`);
+
+
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
