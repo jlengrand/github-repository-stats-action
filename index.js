06a80da2017c6f69083f63083d6f7a36a34d6399
Julien Lengrand-Lambert <julien@lengrand.fr>
2023-06-25 15:39:11 +0200

    ?

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
 
-  const token = core.getInput('access-token');
-  console.log(`The token is ${token}!`);
+async function main() {
 
-  const fullName = github.context.payload.repository.full_name;
-  const [owner, repo] = fullName.split("/");
+  try {
 
-  const octokit = github.getOctokit(token);
+    const token = core.getInput('access-token');
+    console.log(`The token is ${token}!`);
   
-  const data = await octokit.request('GET /repos/{owner}/{repo}/traffic/views', {
-    owner: owner,
-    repo: repo,
-    // headers: {
-    //   'X-GitHub-Api-Version': '2022-11-28'
-    // }
-  })
-
-  console.log(`The data : ${JSON.stringify(data, undefined, 2)}`);
-
-  // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // // Get the JSON context for the event that triggered the workflow
-  // const context = JSON.stringify(github.context, undefined, 2)
-  // console.log(`The event context : ${context}`);
-
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+    const fullName = github.context.payload.repository.full_name;
+    const [owner, repo] = fullName.split("/");
+  
+    const octokit = github.getOctokit(token);
+    
+    const data = await octokit.request('GET /repos/{owner}/{repo}/traffic/views', {
+      owner: owner,
+      repo: repo,
+      // headers: {
+      //   'X-GitHub-Api-Version': '2022-11-28'
+      // }
+    })
+  
+    console.log(`The data : ${JSON.stringify(data, undefined, 2)}`);
+  
+    // `who-to-greet` input defined in action metadata file
+    // const nameToGreet = core.getInput('who-to-greet');
+    // console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // // Get the JSON context for the event that triggered the workflow
+    // const context = JSON.stringify(github.context, undefined, 2)
+    // console.log(`The event context : ${context}`);
+  
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
