4edcfdabc730dd1be9e1be9acda4273f16e0ff7d
julien Lengrand-Lambert <julien@lengrand.fr>
2023-06-29 13:27:09 +0200

    Feat/grabbing repo stats (#1)
    * First version of working action, sending data to server.
    * Adds daily cron job

--- index.js
+++ index.js
@@ -1,15 +1,81 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+import got from 'got';
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function main() {
+
+  try {
+    const time = Date.now();
+
+    let serverUrl = core.getInput('server-url');
+
+    // remove trailing slash
+    if (serverUrl.endsWith('/')) {
+      console.log("slicing");
+      serverUrl = serverUrl.slice(0, -1);
+    }
+    console.log(`The server url is ${serverUrl}!`);
+
+    const token = core.getInput('access-token');
+    console.log(`The token is ${token}!`);
+  
+    const fullName = github.context.payload.repository.full_name;
+    const [owner, repo] = fullName.split("/");
+  
+    console.log(`Found owner and repo : ${owner} and ${repo}`);
+
+    const octokit = github.getOctokit(token);
+    
+    const views = await octokit.request('GET /repos/{owner}/{repo}/traffic/views', {
+      owner: owner,
+      repo: repo,
+    })
+
+    const clones = await octokit.request('GET /repos/{owner}/{repo}/traffic/clones', {
+      owner: owner,
+      repo: repo,
+    })
+
+    await sendViewsStats(serverUrl, owner, repo, views.data);
+    await sendClonesStats(serverUrl, owner, repo, clones.data);
+  
+    const payload = {
+      owner: owner,
+      repo: repo,
+      views: views.data,
+      clones: clones.data,
+      time: time
+    }
+
+    core.setOutput("payload", payload);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+async function sendViewsStats(serverUrl, owner, repo, payload) {
+  const url = `${serverUrl}/api/repositories/${owner}/${repo}/views`;
+  try {
+   const response = await got.post(url, {
+      json: payload,
+   }).json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function sendClonesStats(serverUrl, owner, repo, payload) {
+  const url = `${serverUrl}/api/repositories/${owner}/${repo}/clones`;
+
+  try {
+    const response = await got.post(url, {
+       json: payload,
+    }).json();
+   } catch (error) {
+     console.error(error);
+   }
+}
+
+main();
+
